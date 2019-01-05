3cc933fb43379cfb797485b2232d880b07cc0db4
unknown <kumarpranav1987@gmail.com.com>
2019-01-05 15:09:06 +0530

    First Commit

---
+++ src/main/java/com/livetechstudy/java_livetechstudy/App.java
@@ -0,0 +1,13 @@
+package com.livetechstudy.java_livetechstudy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
